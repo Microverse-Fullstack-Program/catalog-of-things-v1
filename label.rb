b8988d030500311e71ef77e03232b7612fe86b23
cherelemma <chere.lemma@aastu.edu.et>
2023-06-06 14:37:25 +0300

    defined and set up Label properties the constructor method.

--- label.rb
+++ label.rb
@@ -1,3 +1,13 @@
 class Label
+    attr_accessor :title, :color
+    attr_reader :id, :items
+
+    def initialize(title, color)
+        @id = Random.rand(1..1000)
+        @title = title
+        @color = color
+        @items = []
+    end
+
 
 end
