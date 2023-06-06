168c31eddb406e635b113fd6f7ba3f4e44b1ff59
cherelemma <chere.lemma@aastu.edu.et>
2023-06-06 14:37:12 +0300

    defined and set up Book properties the constructor method.

--- book.rb
+++ book.rb
@@ -1,5 +1,12 @@
 require_relative 'item'
 
 class Book < Item
+    attr_accessor :publisher, :cover_state
+
+    def initialize(publisher, cover_state, label, publish_date, archived)
+        super(label, publish_date, archived)
+        @publisher = publisher
+        @cover_sate = cover_sate
+    end
 
 end
