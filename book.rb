aee18e7e68af2f2095fbbf51173ab0730d376da3
cherelemma <chere.lemma@aastu.edu.et>
2023-06-07 03:45:54 +0300

    Update Book class constructor

--- book.rb
+++ book.rb
@@ -1,21 +1,22 @@
-require_relative 'item'
+require_relative './item'
 
 class Book < Item
     attr_accessor :publisher, :cover_state
 
-    def initialize(publisher, cover_state, label, publish_date, archived)
-        super(label, publish_date, archived)
+    def initialize(publisher, cover_state, label, publish_date)
+        super(nil, nil, nil, label, publish_date)
         @publisher = publisher
-        @cover_sate = cover_sate
+        @cover_state = cover_state
+        @archived = can_be_archived?
     end
     
     def can_be_archived?
-        return true if @cover_state == 'bad' || super.can_be_archived?
+        state = @cover_state.to_s
+        return true if super() || state.casecmp?('bad') || state.casecmp?('very bad')
 
         false
     end
 
-    private:
-        can_be_archived?
+    private :can_be_archived?
 
 end
