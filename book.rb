0f720196c675055926353414c180cc651a6c5c3a
cherelemma <chere.lemma@aastu.edu.et>
2023-06-07 10:09:15 +0300

    update Book class constructor

--- book.rb
+++ book.rb
@@ -3,8 +3,8 @@ require_relative './item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state, label, publish_date)
-    super(nil, nil, nil, label, publish_date)
+  def initialize(publisher, cover_state, publish_date = nil)
+    super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
     @archived = can_be_archived?
