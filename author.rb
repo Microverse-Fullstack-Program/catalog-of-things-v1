c9909cd05090ab1ab87e621eb4f0ca2a24614482
danutibe07 <danielutibe07@gmail.com>
2023-06-07 17:40:12 +0100

    Modified Book.rb and Author.rb

--- author.rb
+++ author.rb
@@ -1,24 +1,3 @@
-# class Author
-#   attr_reader :id, :first_name, :last_name, :items
-
-#   def initialize(id, first_name = '', last_name = '')
-#     @id = generate_random_id
-#     @first_name = first_name
-#     @last_name = last_name
-#     @items = []
-#   end
-
-#   def generate_random_id
-#     rand(1..100)
-#   end
-
-#   def add_item(item, game_library)
-#     @items << item
-#     item.author = self
-#     game_library.authors << self
-#   end
-# end
-
 require_relative 'item'
 
 class Author < Item
