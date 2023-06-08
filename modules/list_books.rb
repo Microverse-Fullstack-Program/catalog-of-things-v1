85ac690b010bdc8d14eabc8ca93772cb1f7dbb0f
danutibe07 <danielutibe07@gmail.com>
2023-06-08 20:08:39 +0100

    Corrected Rubocop errors

--- modules/list_books.rb
+++ modules/list_books.rb
@@ -9,7 +9,7 @@ module ListBooks
     puts 'Listing all books:'
     puts '-' * 100
     @books.each do |book|
-      print  ' ' * 5
+      print ' ' * 5
       print "Id: #{book.id}, Publisher: #{book.publisher}, Cover State: #{book.cover_state}, "
       puts "Label: #{book.label}, Publish Date: #{book.publish_date}, Archived: #{book.archived}"
       puts '-' * 100
